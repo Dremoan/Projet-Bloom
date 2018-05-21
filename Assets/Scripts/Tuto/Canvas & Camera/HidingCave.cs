b2a26437ca4648b69781db591ce4dfa0509e1a60
Unknown <Robin.seignolles@hotmail.fr>
2018-05-21 22:35:15 +0200

    C'est bon ça marche !

---
+++ Assets/Scripts/Tuto/Canvas & Camera/HidingCave.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingCave : MonoBehaviour {
+
+	public GameObject grotte;
+	public GameObject skyBox;
+	public PolygonCollider2D colliderLevel;
+	public Animator anim;
+	public Animator animFalaise;
+	public CameraBehavior cameraScript;
+	public Camera mainCamera;
+	private bool canDezoom;
+
+	void Update () 
+	{
+		if(canDezoom)
+		{
+			mainCamera.orthographicSize = Mathf.Lerp (mainCamera.orthographicSize, 150.01f , Time.deltaTime * 3f);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		if(coll.gameObject.tag == "Player")
+		{
+			grotte.SetActive (false);
+			anim.Play ("LightCave");
+			canDezoom = true;
+			cameraScript.XMaxValue = -709f;
+			cameraScript.XMinValue = -1100f;
+			cameraScript.YMaxEnabled = false;
+			cameraScript.YMinEnabled = false;
+			StartCoroutine (ActiveColliderLevel ());
+		}
+	}
+
+	IEnumerator ActiveColliderLevel()
+	{
+		yield return new WaitForSeconds (0.75f);
+		skyBox.SetActive (true);
+		yield return new WaitForSeconds (0.75f);
+		animFalaise.Play ("Falaise qui tombe");
+		colliderLevel.enabled = true;
+	}
+}
---
+++ Assets/Scripts/Tuto/MiniBoss/ProjectileRock.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileRock : Projectile 
+{
+	public CircleCollider2D explosionArea;
+	public float waitTillLaunch = 0.5f;
+	private bool canExplode = true;
+
+	protected override void Shoot()
+	{
+		StartCoroutine (ShootDelay ());
+	}
+
+	IEnumerator removeDrop()
+	{
+		yield return new WaitForSeconds (0.25f);
+		GetComponent<PolygonCollider2D> ().enabled = true;
+		explosionArea.enabled = false;
+		yield return new WaitForSeconds (5f);
+		GetComponent<PolygonCollider2D> ().enabled = false;
+		canExplode = true;
+		projectileBody.mass = 1;
+		DropManagerComponent.RemoveDrop (this);
+	}
+
+	IEnumerator ShootDelay()
+	{
+		this.GetComponent<SpriteRenderer> ().sortingOrder = -4;
+		yield return new WaitForSeconds (waitTillLaunch);
+		if (needForce)
+		{
+			dirToTarget = targetPosition - launchPlace.transform.position;
+			projectileBody.velocity = dirToTarget * shootSpeed * Time.fixedDeltaTime;
+			needForce = false;
+		}
+		if(Vector3.Distance(transform.position, launchPlace.transform.position) > dirToTarget.magnitude && canExplode)
+		{
+			canExplode = false;
+			projectileBody.velocity = Vector2.zero;
+			projectileBody.mass = 100;
+			explosionArea.enabled = true;
+			StartCoroutine (removeDrop ());
+		}
+	}
+}
