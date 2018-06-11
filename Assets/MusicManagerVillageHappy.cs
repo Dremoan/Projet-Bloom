0899e6201886f5f2e7e2d62c9f78e21ee5a1fec4
Unknown <corentin.castric@gmail.com>
2018-06-11 23:56:45 +0200

    Intégration musique Village Happy

---
+++ Assets/MusicManagerVillageHappy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManagerVillageHappy : MonoBehaviour
+{
+
+    public FMOD.Studio.EventInstance backgroundMusic;
+
+    void Start()
+    {
+        backgroundMusic = FMODUnity.RuntimeManager.CreateInstance("event:/VILLAGE/FlowerTown");
+        backgroundMusic.start();
+
+    }
+}
+
+
